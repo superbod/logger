d28c5a43c61105221d459ec94eb5e44d98d08044
Bogdan Chayun <bogdanchauyn@gmail.com>
2024-09-05 23:55:22 +0300

    small fixes

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
     <div class="p-5 mb-4 bg-transparent rounded-3">
         <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!!!@!</h1>
+            <h1 class="display-4">Congratulations!</h1>
             <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
             <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
         </div>
